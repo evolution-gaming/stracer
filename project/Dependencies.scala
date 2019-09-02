1a69a589e30505c5bfaffb4b63dc8bfc4f012953
Yaroslav Klymko <t3hnar@gmail.com>
2019-09-02 14:36:35 +0200

    bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,18 +3,18 @@ import sbt._
 object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"    % "3.0.8"
-  val zipkin        = "io.zipkin.zipkin2"    % "zipkin"       % "2.15.0"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "0.0.23"
+  val zipkin        = "io.zipkin.zipkin2"    % "zipkin"       % "2.16.2"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "0.0.28"
   val random        = "com.evolutiongaming" %% "random"       % "0.0.3"
   val configTools   = "com.evolutiongaming" %% "config-tools" % "1.0.3"
-  val skafka        = "com.evolutiongaming" %% "skafka"       % "6.0.2"
+  val skafka        = "com.evolutiongaming" %% "skafka"       % "6.1.0"
 
   object Cats {
     private val version = "1.6.1"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
+    val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
   }
   
   object Scodec {
@@ -23,7 +23,7 @@ object Dependencies {
   }
 
   object PlayJsonTools {
-    private val version = "0.3.11"
+    private val version = "0.3.12"
     val tools   = "com.evolutiongaming" %% "play-json-tools"   % version
     val generic = "com.evolutiongaming" %% "play-json-generic" % version
   }
