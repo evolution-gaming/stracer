f344d00df2f074559c4832fb29cae8a018e6326c
rfigueiredo <rfigueiredo@evolution-gaming.com>
2019-10-02 11:31:20 +0200

    style: format dependencies and build files

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,17 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
-  val zipkin = "io.zipkin.zipkin2" % "zipkin" % "2.17.0"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "1.0.3"
-  val random = "com.evolutiongaming" %% "random" % "0.0.5"
-  val configTools = "com.evolutiongaming" %% "config-tools" % "1.0.4"
-  val skafka = "com.evolutiongaming" %% "skafka" % "7.0.0"
+  val scalatest     = "org.scalatest"       %% "scalatest"    % "3.0.8"
+  val zipkin        = "io.zipkin.zipkin2"   % "zipkin"        % "2.17.0"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "1.0.3"
+  val random        = "com.evolutiongaming" %% "random"       % "0.0.5"
+  val configTools   = "com.evolutiongaming" %% "config-tools" % "1.0.4"
+  val skafka        = "com.evolutiongaming" %% "skafka"       % "7.0.0"
 
   object Cats {
     private val version = "2.0.0"
-    val core = "org.typelevel" %% "cats-core" % version
+
+    val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
     val effect = "org.typelevel" %% "cats-effect" % "2.0.0"
@@ -24,17 +25,18 @@ object Dependencies {
 
   object PlayJsonTools {
     private val version = "0.3.13"
-    val tools = "com.evolutiongaming" %% "play-json-tools" % version
+
+    val tools   = "com.evolutiongaming" %% "play-json-tools"   % version
     val generic = "com.evolutiongaming" %% "play-json-generic" % version
   }
 
   object Circe {
-    private val version = "0.12.1"
+    private val version       = "0.12.1"
     private val versionExtras = "0.12.2"
 
-    val core = "io.circe" %% "circe-core" % version
-    val generic = "io.circe" %% "circe-generic" % version
-    val parser = "io.circe" %% "circe-parser" % version
+    val core             = "io.circe" %% "circe-core"           % version
+    val generic          = "io.circe" %% "circe-generic"        % version
+    val parser           = "io.circe" %% "circe-parser"         % version
     val `generic-extras` = "io.circe" %% "circe-generic-extras" % versionExtras
 
     val all: Seq[ModuleID] = Seq(core, generic, parser, `generic-extras`)
