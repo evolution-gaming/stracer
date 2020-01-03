794507a5a6a00ad8193bde877dcc7e20dfd8b87c
Yaroslav Klymko <t3hnar@gmail.com>
2020-01-03 21:41:39 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,10 @@ object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"    % "3.1.0"
   val zipkin        = "io.zipkin.zipkin2"   % "zipkin"        % "2.19.2"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "1.4.0"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "1.5.0"
   val random        = "com.evolutiongaming" %% "random"       % "0.0.6"
   val configTools   = "com.evolutiongaming" %% "config-tools" % "1.0.4"
-  val skafka        = "com.evolutiongaming" %% "skafka"       % "9.0.0"
+  val skafka        = "com.evolutiongaming" %% "skafka"       % "9.0.1"
 
   object Cats {
     private val version = "2.0.0"
