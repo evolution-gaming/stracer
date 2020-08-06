5a0220c22f1b798eda3c5c3f4ce812df479b6185
Scala Steward <me@scala-steward.org>
2020-08-06 04:17:24 +0200

    Update zipkin to 2.21.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"    % "3.2.0"
-  val zipkin        = "io.zipkin.zipkin2"   % "zipkin"        % "2.21.5"
+  val zipkin        = "io.zipkin.zipkin2"   % "zipkin"        % "2.21.6"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "2.0.4"
   val random        = "com.evolutiongaming" %% "random"       % "0.0.7"
   val configTools   = "com.evolutiongaming" %% "config-tools" % "1.0.4"
