85cacb270726ffceacb15e972bd698fea4553c5e
Scala Steward <scala_steward@virtuslab.com>
2022-07-15 15:04:30 +0000

    Update scodec-bits to 1.1.34

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
 
   object Scodec {
     val core = "org.scodec" %% "scodec-core" % "1.11.7"
-    val bits = "org.scodec" %% "scodec-bits" % "1.1.18"
+    val bits = "org.scodec" %% "scodec-bits" % "1.1.34"
   }
 
   object PlayJsonTools {
