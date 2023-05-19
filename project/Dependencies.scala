579d1e2d8b0f21ac6827eb3e1a26a93f059a1484
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-05-19 16:55:48 +0200

    bump skafka to 15.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper"  % "3.1.1"
   val random        = "com.evolutiongaming" %% "random"       % "1.0.0"
   val configTools   = "com.evolutiongaming" %% "config-tools" % "1.0.4"
-  val skafka        = "com.evolutiongaming" %% "skafka"       % "14.1.2"
+  val skafka        = "com.evolutiongaming" %% "skafka"       % "15.0.1"
 
 
   object Cats {
